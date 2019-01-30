585a9ab26d62f8ad2824648d7d293025014149c2
Cannicide <41762964+Cannicide@users.noreply.github.com>
2019-01-29 22:49:59 -0500

    Experimental Support of Other Developers' Commands
    Untested addition to allow other developers to create and import their own commands into the official Sif Casino codebase.

---
+++ constants.js
@@ -0,0 +1,9 @@
+//Constant variables - Will not and should not be changed in the code
+var profileStarterAmount1 = 50; //Amount given to new casino profiles to start off
+var helpCommand1 = ""; //Universal content for all variations of the help command
+
+
+module.exports = {
+    profileStarterAmount: profileStarterAmount1,
+    helpCommand: helpCommand1
+}
---
+++ external.js
@@ -0,0 +1,41 @@
+//Used to initialize commands' code files if from an external source
+/* 
+    If you want to help develop command(s) for this bot, this is where your command(s) will be
+    initialized. Each command should have its own JS file unless the entirety of the command can
+    be written neatly in 3 lines or less.
+
+    Create a separate JS file to hold your command in the format style shown in ext-example.js...
+    Then, add your command to the externalCommands object as an embedded object itself, as shown below.
+    Give your embedded object a name property, this will be used to call your command (i.e. if the name
+    is set to "example", it will be called with "/example" by the user.) Finally, create a get property
+    with type Function that will return the stuff that will be sent to the user in a discord message. 
+    You can get the command's arguments (that the user specifies, as shown in the templateExample) 
+    using the variable "args". See the example below or contact Cannicide#2753 for more information.
+*/
+
+const externalCommands = {
+    extExample: {
+        name: "example",
+        get: function() {
+            var example = require("./ext-example"); //Your command's JS file
+            return example.example2("World"); //Content to be sent in the message
+        }
+    },
+    templateExample: {
+        name: "command",
+        get: function() {
+            //var command = require("./your-js-file-without-the-.js-at-the-end");
+            //return command.thingToSendInMessage(args); 
+            //args is sent your function, it is the command's arguments in an array
+            //(i.e. in "/casino profile @user", "profile" and "@user" are pushed into args)
+        }
+    }
+}
+
+module.exports = {
+    commands: externalCommands
+}
+
+//This isn't too complicated when you get the hang of it. The purpose of this is to make importing external
+//authors' codes into this project much easier. Doing this simplifies the depth of my tasks on github
+//and provides support for endless commands from external authors without modifying server.js at all.
