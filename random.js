ebb4f74e25cb55180bd132b20f3773a2ee287546
Cannicide <41762964+Cannicide@users.noreply.github.com>
2019-02-16 14:11:31 -0500

    Random Update and Assorted Bug Fixes
    Fixed several bugs, and changed the random number function to the Versailles algorithm.

--- random.js
+++ random.js
@@ -1,4 +1,5 @@
 //Algorithm to return random numbers
+//Random Edition: versaillesRandAlgorithm-v1.4
 
 function rand(min, max) {
     var numbers = [];
@@ -8,10 +9,10 @@ function rand(min, max) {
     var result;
     var rnd1, rnd2, rnd3;
     function internalRand(maxx, minn) {
-        rnd1 = Math.floor(Math.random() * (maxx + 1)) + minn;
-        rnd2 = Math.floor(Math.random() * (maxx + 1)) + minn;
-        rnd3 = Math.floor(Math.random() * (maxx + 1)) + minn;
-        var res = (rnd1 + rnd2 + rnd3) / 3;
+        rnd1 = Math.floor(Math.random() * (Math.round(maxx / 3) + 1)) + minn;
+        rnd2 = Math.floor(Math.random() * (Math.round(maxx / 3) + 1)) + minn;
+        rnd3 = Math.floor(Math.random() * (Math.round(maxx / 3) + 1)) + minn;
+        var res = (rnd1 + rnd2 + rnd3);
         return Math.round(res);
     }
     var isBad = true;
