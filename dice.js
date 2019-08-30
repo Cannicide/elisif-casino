3a0f22d811feafe89d84321dd48b4ff24bd946f3
Cannicide <41762964+Cannicide@users.noreply.github.com>
2019-08-30 17:27:05 -0400

    Dice Balance Updates
    Lowered the multipliers of rewards of Dice:
    Snake Eyes rewards went from x3 to x2
    Double Number rewards went from x7 to x3

--- dice.js
+++ dice.js
@@ -13,14 +13,14 @@ function dice(args, ifprofile, prefix, message) {
           var roll1 = rand.num(1, 6);
           var roll2 = rand.num(1, 6);
           if (roll1 == 1 && roll2 == 1) {
-              amount = amount * 3;
+              amount = amount * 2;
               ls.set(message.author.id + "profile", Number(ls.get(message.author.id + "profile")) + amount);
-              return "You rolled snake eyes (1 and 1) and gained **3 times your original bet!**";  
+              return "You rolled snake eyes (1 and 1) and gained **2 times your original bet!**";  
           }
           else if (roll1 == roll2) {
-            amount = amount * 7;
+            amount = amount * 3;
             ls.set(message.author.id + "profile", Number(ls.get(message.author.id + "profile")) + amount);
-            return "It's a match, " + message.author.username + "! You rolled double " + roll1 + "s and gained **7 times your original bet!**";
+            return "It's a match, " + message.author.username + "! You rolled double " + roll1 + "s and gained **3 times your original bet!**";
           }
           else {
             ls.set(message.author.id + "profile", Number(ls.get(message.author.id + "profile")) - amount);
