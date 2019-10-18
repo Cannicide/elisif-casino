009c5a82d780ed26a47732013fd8365cedfc48d8
Cannicide <41762964+Cannicide@users.noreply.github.com>
2019-10-18 13:10:15 -0400

    Introducing Shop.js
    Sif Casino Shop - Coming soon
    We'll make your hard-earned gambling worth the risk, with shop rewards.

---
+++ shop.js
@@ -0,0 +1,2 @@
+//This feature, coming soon, will be a shop where customized items, tags, etc. can be "bought" using the bot's Sif Coins currency.
+//The development of this shop will be heavily dependent on another project I am working on called Webset, which will be the hosted API making this possible
