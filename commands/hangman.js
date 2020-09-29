3fa70456ac5512345ae707302ad75f3104a1b712
Cannicide <41762964+Cannicide@users.noreply.github.com>
2020-09-28 23:33:27 -0400

    Fixed hangman, nerfed reward to $2500, added elisif-themed words.

--- commands/hangman.js
+++ commands/hangman.js
@@ -6,9 +6,68 @@ var Profile = require("./profile").Profile;
 var Alias = require("../alias");
 var settings = require("../settings");
 
+var words = [
+    "Weather", 
+    "Incredibility", 
+    "Ponderous", 
+    "Fastidious", 
+    "Ominous", 
+    "Capricious", 
+    "Pervasive", 
+    "Aloof", 
+    "Disseminate", 
+    "Pugnacious", 
+    "Whimsical", 
+    "Unfathomable", 
+    "Predilection", 
+    "Insurgency", 
+    "Inadequate", 
+    "Immeasurable", 
+    "Terracotta", 
+    "Significance", 
+    "Immobility", 
+    "Versatility", 
+    "Carcinogen", 
+    "Death", 
+    "Hanged", 
+    "Eejit", 
+    "Magnanimous", 
+    "Pneumonoultramicroscopicsilicovolcanoconiosis", 
+    "Pneumonoultramicroscopicsilicovolcanoconiosis", 
+    "Pneumonoultramicroscopicsilicovolcanoconiosis", 
+    "Floccinaucinihilipilification", 
+    "Spectrophotofluorometrically", 
+    "Euouae", 
+    "Honorificabilitudinitatibus", 
+    "Ragamuffin", 
+    "Discombobulate", 
+    "Cat", 
+    "Ban", 
+    "Cabotage", 
+    "Bibble", 
+    "Erinaceous", 
+    "Firman", 
+    "Gabelle", 
+    "Kakorrhaphiophobia", 
+    "Nudiustertian", 
+    "Xertz", 
+    "Yarborough", 
+    "AlJumahiriyahAlArabiyahAlLibiyahAshShabiyahAlIshtirakiyahAlUzma", 
+    "Texas", 
+    "Michigan", 
+    "NewYork", 
+    "Botanical", 
+    "Garden", 
+    "Dollars", 
+    "Cash", 
+    "Encephalosis",
+    "SifCasino",
+    "Elisif"
+];
+
 function hangmanStart(prefix, message) {
     if (ls.get(message.guild.id + "hangmanGame")) {
-        return "You have already started a game of hangman! Type `" + prefix + "hm end` to end the game early.";
+        return message.channel.send("You have already started a game of hangman! Type `" + prefix + "hm end` to end the game early.");
     }
     else {
         var hObj = {
@@ -19,7 +78,7 @@ function hangmanStart(prefix, message) {
             participants: []
         }
         hObj.started = true;
-        var words = ["Weather", "Incredibility", "Ponderous", "Fastidious", "Ominous", "Capricious", "Pervasive", "Aloof", "Disseminate", "Pugnacious", "Whimsical", "Unfathomable", "Predilection", "Insurgency", "Inadequate", "Immeasurable", "Terracotta", "Significance", "Immobility", "Versatility", "Carcinogen", "Death", "Hanged", "Eejit", "Magnanimous", "Pneumonoultramicroscopicsilicovolcanoconiosis", "Pneumonoultramicroscopicsilicovolcanoconiosis", "Pneumonoultramicroscopicsilicovolcanoconiosis", "Floccinaucinihilipilification", "Spectrophotofluorometrically", "Euouae", "Honorificabilitudinitatibus", "Ragamuffin", "Discombobulate", "Cat", "Ban", "Cabotage", "Bibble", "Erinaceous", "Firman", "Gabelle", "Kakorrhaphiophobia", "Nudiustertian", "Xertz", "Yarborough", "AlJumahiriyahAlArabiyahAlLibiyahAshShabiyahAlIshtirakiyahAlUzma", "Texas", "Michigan", "NewYork", "Botanical", "Garden", "Dollars", "Cash", "Encephalosis"];
+        
         var wordnum = Math.floor(Math.random() * words.length);
         var word = words[wordnum];
         var underscores = "";
@@ -31,7 +90,7 @@ function hangmanStart(prefix, message) {
         hObj.guesses = 12;
         hObj.participants.push(message.author.id);
         ls.set(message.guild.id + "hangmanGame", hObj);
-        return "You have begun a game of hangman! Use `" + prefix + "hm guess` to begin guessing!\n\n" + underscores;
+        message.channel.send("You have begun a game of hangman! Use `" + prefix + "hm guess` to begin guessing!\n\n" + underscores);
     }
 }
 
@@ -39,14 +98,14 @@ function hangmanEnd(message) {
     if (ls.exist(message.guild.id + "hangmanGame")) {
         if (ls.get(message.guild.id + "hangmanGame").participants[0] == message.author.id || message.member.hasPermission("ADMINISTRATOR")) {
             ls.remove(message.guild.id + "hangmanGame");
-            return `Ended hangman game, ${message.author.username}!`;
+            message.channel.send(`Ended hangman game, ${message.author.username}!`);
         }
         else {
-            return `${message.author.username}, you do not have sufficient permission to end the current hangman game. Only the guild member who started the game and/or the admins of the guild can end a hangman game.`;
+            message.channel.send(`${message.author.username}, you do not have sufficient permission to end the current hangman game. Only the guild member who started the game and/or the admins of the guild can end a hangman game.`);
         }
     }
     else {
-        return `${message.author.username}, you do not have any running games to end.`;
+        message.channel.send(`${message.author.username}, you do not have any running games to end.`);
     }
 }
 
@@ -61,7 +120,7 @@ function hangmanGuess(args, prefix, message) {
         var gotLetter = false;
         if (Number(limit) <= 1) {
             ls.remove(message.guild.id + "hangmanGame");
-            return `Sorry ${message.author.username}, you are out of guesses. **Game over!**\n` + "The word was: **" + word.split("").join(" ") + "**";
+            return message.channel.send(`Sorry ${message.author.username}, you are out of guesses. **Game over!**\n` + "The word was: **" + word.split("").join(" ") + "**");
         }
         else {
         hObj.participants.forEach(function(member, index) {
@@ -94,25 +153,25 @@ function hangmanGuess(args, prefix, message) {
         if (gotLetter) {
             if (underscores.indexOf("⬜") < 0) {
                 hObj.participants.forEach(function(member, index) {
-                    var subprof = new Profile(member);
+                    var subprof = new Profile(message.guild.members.find(m => m.id == member).lastMessage);
                     subprof.getBal();
 
                     subprof.add(2500);
                 });
                 ls.remove(message.guild.id + "hangmanGame");
-                return "You won hangman! **+5000 dollars to all participants!**";
+                message.channel.send("You won hangman! **+2500 dollars to all participants!**");
             }
         }
         else {
             limit = Number(limit) - 1
             hObj.guesses = limit;
             ls.set(message.guild.id + "hangmanGame", hObj)
-            return `Incorrect guess, ${message.author.username}... ` + limit + " guesses left.";
+            message.channel.send(`Incorrect guess, ${message.author.username}... ` + limit + " guesses left.");
         }
         }
     }
     else {
-        return "You must start a game before guessing the word! Use `" + prefix + "hm start` to get started!";
+        message.channel.send("You must start a game before guessing the word! Use `" + prefix + "hm start` to get started!");
     }
 }
 
@@ -130,7 +189,7 @@ function hangmanDo(args, message) {
     var subinput = [false, args[1]];
 
     if (!action) {
-        return `${message.author.username}, please specify whether to start a hangman game, end it, or guess in it.`;
+        message.channel.send(`${message.author.username}, please specify whether to start a hangman game, end it, or guess in it.`);
     }
 
     if (action.toLowerCase() == "start") {
@@ -141,14 +200,14 @@ function hangmanDo(args, message) {
             return hm.guess(subinput, prefix, message);
         }
         else {
-            return `${message.author.username}, please specify a valid letter to guess.`;
+            message.channel.send(`${message.author.username}, please specify a valid letter to guess.`);
         }
     }
     else if (action.toLowerCase() == "end") {
         return hm.end(message);
     }
     else {
-        return `Please specify a valid statement (start|guess|end) to the command. See the Hangman section of \`${prefix}casino\` for more information.`;
+        message.channel.send(`Please specify a valid statement (start|guess|end) to the command. See the Hangman section of \`${prefix}casino\` for more information.`);
     }
 }
 
@@ -156,7 +215,7 @@ module.exports = {
     commands: [
         new Command("hm", (message, args) => {
 
-            message.channel.send(hangmanDo(args, message));
+            hangmanDo(args, message);
 
         }, false, false, "Starts a guild-based hangman game, ends it, or guesses a letter in it, respectively. All participants gain $2500 dollars each upon victory.").attachArguments([
             {
